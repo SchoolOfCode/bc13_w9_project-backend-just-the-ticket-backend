dbd027d4a2a1bc09415985cb297a00b6b1c21cf2
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2022-11-23 09:37:17 +0000

    created get by ID,post

--- routes/ticketRouter.js
+++ routes/ticketRouter.js
@@ -8,4 +8,13 @@ ticketRouter.get("/", async function (req, res){
     res.status(200).json(allTickets);
 });
 
+ticketRouter.get("/:id", async function (req, res){
+    const ticket = await getTicketById(req.params.id);
+    res.json({success: true, payload: result})
+})
+
+ticketRouter.post("/", async function (req, res){
+    const newTicket = req.bodyconst result = await createTicket(newTicket)
+    res.json({success: true, payload: result})
+})
 export default ticketRouter;
